4b26bd6b9ac912e97f4cc2f4b3055a6b1aee6537
Glaucio Daniel <glauciodaniel@gmail.com>
2020-05-07 23:22:10 -0300

    Site Desktop

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,33 @@
 const body = document.querySelector('body');
 const btOpen = document.getElementById('btn-open');
 const closeMenu = document.querySelectorAll('[data-close="menu"]');
+const btnFooter = document.querySelector('#footer button');
 
-btOpen.addEventListener('click', e=> {
+btnFooter.addEventListener('click', e => {
+    console.log('Protesto Meritíssimo');
+    window.scrollTo(0, 0);
+})
+
+
+
+
+btOpen.addEventListener('click', e => {
     body.classList.add('open-menu');
 })
 
 
-closeMenu.forEach( el => {
+closeMenu.forEach(el => {
     el.addEventListener('click', e => {
         body.classList.remove('open-menu');
     })
 })
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 44.531274, lng: 10.868766 },
+        zoom: 14
+    });
+}
+
 
